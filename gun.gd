e7e75d45ac8d317116bf20439a83eabba80b2fa9
srb <74992183+minusonee@users.noreply.github.com>
2024-10-18 20:43:41 +0300

    feat: added gun pivot and mouse shooting mechanic

--- gun.gd
+++ gun.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+var shootcooldown = false
+
 func _physics_process(delta):
-	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range.size() > 0:
-		var target_enemy = enemies_in_range[0]
-		look_at(target_enemy.global_position)
+	var mouse_position = get_global_mouse_position()
+	look_at(mouse_position)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && shootcooldown == true:
+		shoot()
+		shootcooldown = false
 
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
@@ -13,5 +16,7 @@ func shoot():
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
 
+
 func _on_timer_timeout() -> void:
-	shoot()
+	shootcooldown = true
+	$Timer.start()
