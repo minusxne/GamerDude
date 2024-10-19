027f5330f648fe69218501595e4a6994f2c7daf0
srb <74992183+minusonee@users.noreply.github.com>
2024-10-19 14:29:26 +0300

    feature: added sprite animations

--- player.gd
+++ player.gd
@@ -3,18 +3,44 @@ extends CharacterBody2D
 signal health_depleted
 
 var health = 100
+var last_direction = Vector2(0, 1)
 
 func _physics_process(delta):
-	#move_and_collide(velocity * delta)
-	move_and_slide()
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
-	
+	move_and_slide()
+
 	const DAMAGE_RATE = 5.0
-	
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
-	if overlapping_mobs.size()>0:
-		health-=DAMAGE_RATE*overlapping_mobs.size()*delta
-		%ProgressBar.value=health
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
 		if health <= 0:
 			health_depleted.emit()
+	
+	animations()
+
+func animations():
+	print("Velocity X: ", velocity.x, " | Velocity Y: ", velocity.y)
+	
+	if velocity.length() > 0:
+		last_direction = velocity.normalized()
+		if abs(velocity.x) > abs(velocity.y):
+			if velocity.x > 0:
+				$Player_Sprite.play("walk_right")
+			else:
+				$Player_Sprite.play("walk_left")
+		else:
+			if velocity.y > 0:
+				$Player_Sprite.play("walk_down")
+			else:
+				$Player_Sprite.play("walk_up")
+	else:
+		if last_direction.x > 0:
+			$Player_Sprite.play("default_right")
+		elif last_direction.x < 0:
+			$Player_Sprite.play("default_left")
+		elif last_direction.y > 0:
+			$Player_Sprite.play("default_down")
+		else:
+			$Player_Sprite.play("default_up")
