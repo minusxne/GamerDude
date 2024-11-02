a259e88696679a5b215d95c56f77509c46431a7e
minusonee <74992183+minusonee@users.noreply.github.com>
2024-11-02 16:55:50 +0200

    feat: added bullet bounce POC

--- weapons/bullets/red_bullet.gd
+++ weapons/bullets/red_bullet.gd
@@ -1,25 +1,42 @@
 extends Area2D
 
 var travelled_distance = 0
-var SPEED = 1000
 var RANGE = 1200
+var speed = 1000
+var velocity = Vector2.ZERO
+var bounce_factor = 1.0  # Amount of velocity retained after bouncing (1.0 = full bounce)
 
-func _physics_process(delta):
-	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction*SPEED*delta
-	travelled_distance+=SPEED*delta
-	if travelled_distance>RANGE:
-		queue_free()
+func _ready():
+	velocity = Vector2(speed, 0).rotated(rotation)
+
+func _process(delta):
+	position += velocity * delta
+	#var direction = Vector2.RIGHT.rotated(rotation)
+	#position += direction*SPEED*delta
+	#
+	#
+	#travelled_distance+=SPEED*delta
+	#if travelled_distance>RANGE:
+		#queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-	$Timer.start()
-	SPEED=0
-	%Projectile.play("explode")
-	%Projectile.scale = Vector2(0.5, 0.5)
+	# Get the collision normal (the direction the bullet hit)
+	var collision_normal = (body.global_position - global_position).normalized()
+	
+	# Reflect the velocity to create a bounce effect
+	velocity = velocity.bounce(collision_normal) * bounce_factor
+	
+	#$Timer.start()
+	#
+	#SPEED=0
+	#
+	#%Projectile.play("explode")
+	#
+	#%Projectile.scale = Vector2(0.5, 0.5)
+	#
 	if body.has_method("take_damage"):
 		body.take_damage()
 		body.take_damage()
-
 
 func _on_timer_timeout() -> void:
 	queue_free()
